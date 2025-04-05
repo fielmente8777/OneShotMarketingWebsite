9e41455ea1102a771c8da77cc4a0d54c3cc53843
sumit <sk7627577@gmail.com>
2025-04-05 09:29:55 +0530

    Refactor generateStaticParams and generateMetadata functions for improved clarity and structure

--- src/app/services/[...slugs]/page.tsx
+++ src/app/services/[...slugs]/page.tsx
@@ -1,38 +1,38 @@
 import { pageData2 } from "@/data/slugData";
 
 interface Params {
-  params: {
+  params: Promise<{
     slugs: string[];
-  };
+  }>;
 }
 
-// export async function generateStaticParams() {
-//   const datas = pageData2;
-//   console.log("datas", datas);
-//   const dataList = pageData2;
-//   // console.log("dataList", dataList);
-//   return dataList.map((data) => ({
-//     slugs: data.slug,
-//   }));
-// }
+export async function generateStaticParams() {
+  const datas = pageData2;
+  console.log("datas", datas);
+  const dataList = pageData2;
+  // console.log("dataList", dataList);
+  return dataList.map((data) => ({
+    slugs: data.slug,
+  }));
+}
 
-// export async function generateMetadata(props: Params) {
-//   const slugs = await props.params;
+export async function generateMetadata(props: Params) {
+  const slugs = await props.params;
 
-//   const slugsSlug = await slugs.slugs;
-//   const slugsData = pageData2.find((data) => data.slug === slugsSlug);
+  const slugsSlug = await slugs.slugs;
+  const slugsData = pageData2.find((data) => data.slug === slugsSlug);
 
-//   if (!slugsData) {
-//     return {
-//       title: "slugs We Serve",
-//       description: "slugs We Serve",
-//     };
-//   }
-//   return {
-//     title: slugsData.title,
-//     description: slugsData.title,
-//   };
-// }
+  if (!slugsData) {
+    return {
+      title: "slugs We Serve",
+      description: "slugs We Serve",
+    };
+  }
+  return {
+    title: slugsData.title,
+    description: slugsData.title,
+  };
+}
 
 const page = async (props: Params) => {
   const data = await props.params;

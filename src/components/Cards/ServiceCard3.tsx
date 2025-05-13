c7190671ab5f54da72dc0a0874762b11b358b1af
sumit <sk7627577@gmail.com>
2025-05-13 12:25:15 +0530

    update links

--- src/components/Cards/ServiceCard3.tsx
+++ src/components/Cards/ServiceCard3.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { JSX } from "react";
 export interface ServiceCardProps {
   title: string;
@@ -6,6 +7,7 @@ export interface ServiceCardProps {
   level?: 1 | 2 | 3 | 4 | 5 | 6;
   titlePx?: boolean;
   src?: string;
+  href?: string;
 }
 const ServiceCard3: React.FC<ServiceCardProps> = ({
   title,
@@ -13,10 +15,15 @@ const ServiceCard3: React.FC<ServiceCardProps> = ({
   level,
   titlePx = false,
   src,
+  href,
 }) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+  const CardLink = href ? Link : ("div" as keyof JSX.IntrinsicElements);
   return (
-    <div className="w-full bg-white rounded-2xl border border-primary md:hover:box-shadow duration-300 transition-all ease-in-out flex flex-col items-center justify-center gap-4">
+    <CardLink
+      href={href || "#"}
+      className="w-full bg-white rounded-2xl border border-primary md:hover:box-shadow duration-300 transition-all ease-in-out flex flex-col items-center justify-center gap-4"
+    >
       {src && (
         <div className="relative w-full md:aspect-[4/2.5] aspect-[4/2.5] overflow-hidden rounded-2xl">
           <Image src={src} alt={title} fill className="object-cover" />
@@ -29,9 +36,11 @@ const ServiceCard3: React.FC<ServiceCardProps> = ({
           {title}
         </Tag>
         <div className="w-full h-[1px] bg-secondary max-w-[6.5rem]"></div>
-        <p className="md:heading4 text-lg max-lg:px-4 text-center text-light">{desc}</p>
+        <p className="md:heading4 text-lg max-lg:px-4 text-center text-light">
+          {desc}
+        </p>
       </div>
-    </div>
+    </CardLink>
   );
 };
 
--- src/data/links.tsx
+++ src/data/links.tsx
@@ -267,17 +267,17 @@ export const FooterLinks: FooterLinksProps[] = [
 export const SocialLinks: SocialLinksProps[] = [
   {
     name: "facebook",
-    href: "https://www.facebook.com/",
+    href: "https://www.facebook.com/oneshotsales",
     icon: <FacebookIcon />,
   },
   {
     name: "instagram",
-    href: "https://www.instagram.com/",
+    href: "https://www.instagram.com/oneshot_marketing/",
     icon: <InstagramIcon />,
   },
   {
     name: "linkedin",
-    href: "https://www.linkedin.com/",
+    href: "https://www.linkedin.com/company/oneshotmarketing",
     icon: <LinkedInIcon />,
   },
 ];
